4acc84443eeec236fde06b586a26b7a7cb099315
Dave Compton <davecompton7@gmail.com>
2015-12-13 22:42:19 +0000

    now using latest ghc, ghcjs, ghcjs-dom

--- Main.hs
+++ Main.hs
@@ -1,12 +1,11 @@
-import GHCJS.DOM
-import GHCJS.DOM.Document
-import GHCJS.DOM.HTMLElement
-import GHCJS.DOM.Element
-import GHCJS.DOM.HTMLButtonElement
-import GHCJS.DOM.Node
-import GHCJS.Types
-import GHCJS.Foreign
-import Control.Monad.Trans
+import GHCJS.DOM (runWebGUI, webViewGetDomDocument)
+import GHCJS.DOM.Document (getBody, createElement)
+import GHCJS.DOM.Element (click, setInnerHTML)
+import GHCJS.DOM.HTMLButtonElement (castToHTMLButtonElement)
+import GHCJS.DOM.Node (appendChild)
+import GHCJS.DOM.EventM (on)
+import Control.Monad.Trans (liftIO)
+import Data.JSString (JSString, pack)
 
 foreign import javascript unsafe 
    "alert($1)" 
@@ -14,8 +13,8 @@ foreign import javascript unsafe
 
 main = runWebGUI $ \win -> do
         Just doc <- webViewGetDomDocument win
-        Just body <- documentGetBody doc
-        Just button <- fmap castToHTMLButtonElement <$> documentCreateElement doc "button"
-        nodeAppendChild body $ Just button
-        elementOnclick button $ liftIO $  alert $ toJSString "OUCH!"
-        htmlElementSetInnerHTML button "voila!"
+        Just body <- getBody doc
+        Just button <- fmap castToHTMLButtonElement <$> createElement doc (Just "button")
+        appendChild body $ Just button
+        on button click $ liftIO $ alert $ pack "OUCH!" 
+        setInnerHTML button (Just "voila!")
